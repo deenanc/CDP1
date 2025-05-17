d0e04c8d1ea075c7cd73d1bbaf88fe99dcf48cbb
ddnc1 <ddnc1@LAPTOP-LS9EEU1T>
2025-05-17 17:15:20 +0530

    commit 5/17

--- src/test/java/com/cdp/hooks/Hooks.java
+++ src/test/java/com/cdp/hooks/Hooks.java
@@ -1,12 +1,20 @@
 package com.cdp.hooks;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.cdp.configuration.DriverFactory;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 
 public class Hooks {
@@ -24,6 +32,16 @@ public class Hooks {
 	@After
 	public void tearUp() {
 		driver.quit();
+	}
+	
+	@AfterStep
+	public void AddScreenshot(Scenario scenario) throws IOException {
+		if(scenario.isFailed()) {
+			File soursePath=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			byte[] fileContent=FileUtils.readFileToByteArray(soursePath);
+			scenario.attach(fileContent, "image/png", "image"
+					);
+		}
 	}
 	
 	public static WebDriver getDriver() {
